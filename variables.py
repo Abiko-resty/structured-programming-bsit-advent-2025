56c9e464239982270827f9bba5b2095d955bd63e
Abiko-resty <abikoresty5@gmail.com>
2025-06-04 15:16:45 +0100

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,26 @@
+###Assinging
+## data type
+
+## value
+
+""" 
+intergers
+floats
+strings/characters
+Boolean
+"""
+
+
+number_one =30  
+number_two = 3.17 
+last_name = "Resty"
+first_name = 'Jane'
+is_attentive = False
+is_present = True
+
+## checking for the datatype
+print(type(number_two))
+
+## conversions
+new_number_two = int(number_two)
+print(f"integer:{new_number_two} datatype)
